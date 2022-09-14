11618008f228fe19fbe3ba29abd887155588ed0f
sccmentor <pwinstanley@gmail.com>
2022-09-14 15:48:56 +0100

    Add files via upload

---
+++ RevertToDigitalLicence.ps1
@@ -0,0 +1,8 @@
+﻿$CheckForGVLK = Get-WmiObject SoftwareLicensingProduct -Filter "ApplicationID = '55c92734-d682-4d71-983e-d6ec3f16059f' and LicenseStatus = '5'"
+$CheckForGVLK = $CheckForGVLK.ProductKeyChannel
+
+if ($CheckForGVLK -eq 'Volume:GVLK'){
+
+    $GetDigitalLicence = (Get-WmiObject -query 'select * from SoftwareLicensingService’).OA3xOriginalProductKey
+    cscript c:\windows\system32\slmgr.vbs -ipk $GetDigitalLicence
+}
